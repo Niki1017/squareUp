293a2ade2ea4ff3472b8826f67ed940671885f94
Nikita <ne60201@gmail.com>
2025-07-18 13:14:43 +0500

    базовая настройка pages страниц + подключение HashRouter

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import './css/Header.css';
+import { Link } from 'react-router-dom';
+
+export default function Header() {
+    return (
+        <header>
+            <div className='container'>
+                <h1>Ghbdtn</h1>
+            </div>
+        </header>
+    );
+}
